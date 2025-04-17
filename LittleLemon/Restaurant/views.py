473c58b5a4b12250f378baffad6106078a375a8d
thatsninou <nourkhelfallah3@gmail.com>
2025-04-17 14:56:11 +0100

    third commit

--- LittleLemon/Restaurant/views.py
+++ LittleLemon/Restaurant/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
+
+
+class bookingview(APIView):
+
+    def get(self, request):
+        items = Booking.objects.all()
+        serializer = BookingSerializer(items, many=True)
+        return Response(serializer.data)
+    
+
+class menuview(APIView):
+    
+    def get(self, request):
+        items = Menu.objects.all()
+        serializer = MenuSerializer(items, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = MenuSerializer(data = request.data)
+        
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status': "success", "data": serializer.data})
